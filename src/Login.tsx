5584346bb7f6a94408f276c718f028f61386bbf5
8790fahad <8790fahadado@gmail.com>
2023-04-25 22:53:00 +0100

    chnages

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,43 +1,47 @@
 import React from "react";
-
+import "./styles.css";
+import img from "./img/logo.png";
 const Login = () => {
   return (
-    <div className="container">
-      <h2>Login</h2>
-      <form>
-        <div className="form-group">
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-          />
-          <small id="emailHelp" className="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Password"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-        <hr />
-        <button type="button" className="btn btn-outline-primary">
-          Login with Facebook
-        </button>
-        <button type="button" className="btn btn-outline-danger">
-          Login with Google
-        </button>
-      </form>
+    <div className=" container">
+      <img src={img} alt="logo" className="m-4 img" />
+      <div className="mtt">
+        <h2 className="mt-4 mb-2">Welcome back</h2>
+        <form className="_container">
+          <div className="form-group">
+            <label >Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+          <hr />
+          <button type="button" className="btn btn-outline-primary">
+            Login with Facebook
+          </button>
+          <button type="button" className="btn btn-outline-danger">
+            Login with Google
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
